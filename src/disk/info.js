53688d6293887ed5410da08fa945265b0c5c2507
Zachariah Wadler <zachariah.wadler@gmail.com>
2013-09-12 16:14:49 -0400

    Working on implementing disk.info

--- src/disk/info.js
+++ src/disk/info.js
@@ -3,10 +3,17 @@
         requiresInput: true,
         cmd: {
             linux: function(args, pipe, input, callback) {
-                execCommand('udevadm info -q property -n ' + ,
-                    function(stdout) {
-                        nextCommand(pipe, stdoutToArray(stdout), callback);
-                    });
+                nextCommand([['is.dev', input]], function(isDev) {
+                    if (isDev) {
+                        execCommand('udevadm info -q property -n ' + input,
+                            function(stdout) {
+                                nextCommand(pipe, stdoutToArray(stdout), callback);
+                            });
+                    } else {
+                        bugOut('The provided input "' + input + '" is not a ' +
+                            'valid device name for the command "disk.info"');
+                    }
+                });
             },
             windows: function(args, pipe, input, callback) {
                 execCommand('wmic logicaldisk get name',
